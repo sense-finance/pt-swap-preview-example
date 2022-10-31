d4a8ebc519694c4395fb548724a134579407cd84
Josh Levine <joshuapark95@gmail.com>
2022-10-31 14:18:49 -0400

    feat: add basic test example underlying swap

--- src/SwapPreview.sol
+++ src/SwapPreview.sol
@@ -2,15 +2,11 @@
 pragma solidity ^0.8.13;
 
 import { ERC20 } from "solmate/tokens/ERC20.sol";
+import { FixedPointMathLib } from "solmate/utils/FixedPointMathLib.sol";
 
 interface SpaceLike {
-    enum SwapKind {
-        GIVEN_IN,
-        GIVEN_OUT
-    }
-
     struct SwapRequest {
-        SwapKind kind;
+        BalancerVaultLike.SwapKind kind;
         ERC20 tokenIn;
         ERC20 tokenOut;
         uint256 amount;
@@ -27,6 +23,25 @@ interface SpaceLike {
         uint256 currentBalanceTokenIn,
         uint256 currentBalanceTokenOut
     ) external view returns (uint256);
+
+    function getPoolId() external view returns (bytes32);
+    function pti() external view returns (uint256);
+}
+
+interface BalancerVaultLike {
+    enum SwapKind {
+        GIVEN_IN,
+        GIVEN_OUT
+    }
+
+    function getPoolTokens(bytes32 poolId)
+        external
+        view
+        returns (
+            ERC20[] memory tokens,
+            uint256[] memory balances,
+            uint256 maxBlockNumber
+        );
 }
 
 interface SpaceFactoryLike {
@@ -45,12 +60,34 @@ interface AdapterLike {
 }
 
 contract SwapPreview {
-    function previewUnderlyingIn(address adapter, uint256 maturity, uint256 amount) public returns (uint256) {
-        SpaceLike space = SpaceFactoryLike(AddressBook.SPACE_FACTORY_1_3_0).pools(adapter, maturity);
-        // adapter.scale()
-        // vault.getPoolTokens()
-        // pool.onSwap
-        // return ptsOut;
+    using FixedPointMathLib for uint256;
+
+    function previewUnderlyingIn(address adapter, uint256 maturity, uint256 amountUnderlying) public returns (uint256 ptsOut) {
+        SpaceLike space = AddressBook.SPACE_FACTORY_1_3_0.pools(adapter, maturity);
+        uint256 scale = AdapterLike(adapter).scale();
+        bytes32 poolId = space.getPoolId();
+        uint256 pti = space.pti();
+
+        (ERC20[] memory tokens, uint256[] memory balances, ) = AddressBook.BALANCER_VAULT.getPoolTokens(poolId);
+
+        uint256 amountTarget = amountUnderlying.divWadDown(scale);
+
+        ptsOut = space.onSwap(
+            SpaceLike.SwapRequest({
+                kind: BalancerVaultLike.SwapKind.GIVEN_IN,
+                tokenIn: tokens[1 - pti],
+                tokenOut: tokens[pti],
+                amount: amountTarget,
+                poolId: poolId,
+                // The following are unused for previews
+                lastChangeBlock: 0,
+                from: address(0),
+                to: address(0),
+                userData: ""
+            }),
+            balances[1 - pti],
+            balances[pti]
+        );
     }
 
 }
@@ -58,6 +95,6 @@ contract SwapPreview {
 library AddressBook {
     address public constant DIVIDER_1_3_0 = 0x86bA3E96Be68563E41c2f5769F1AF9fAf758e6E0;
     address public constant PERIPHERY_1_3_0 = 0xFff11417a58781D3C72083CB45EF54d79Cd02437;
-    address public constant SPACE_FACTORY_1_3_0 = 0x5f6e8e9C888760856e22057CBc81dD9e0494aA34;
-    address public constant BALANCER_VAULT = 0xBA12222222228d8Ba445958a75a0704d566BF2C8;
+    SpaceFactoryLike public constant SPACE_FACTORY_1_3_0 = SpaceFactoryLike(0x5f6e8e9C888760856e22057CBc81dD9e0494aA34);
+    BalancerVaultLike public constant BALANCER_VAULT = BalancerVaultLike(0xBA12222222228d8Ba445958a75a0704d566BF2C8);
 }
--- test/SwapPreview.t.sol
+++ test/SwapPreview.t.sol
@@ -7,12 +7,21 @@ import "../src/SwapPreview.sol";
 contract SwapPreviewTest is Test {
     SwapPreview public swapPreview;
 
+    address public constant MA_DAI_ADAPTER = 0x9887e67AaB4388eA4cf173B010dF5c92B91f55B5;
+    uint256 public constant MA_DAI_SERIES_1 = 1688169600;
+
+
     function setUp() public {
         swapPreview = new SwapPreview();
     }
 
-    // function testIncrement() public {
-    //     counter.increment();
-    //     assertEq(counter.number(), 1);
-    // }
+    function testSwaUnderlyingInPreview() public {
+        uint256 PREVIEW_AMT = 1.1e18; // 1.1 DAI
+
+        emit log_named_decimal_uint("DAI in", PREVIEW_AMT, 18);
+
+        uint256 ptsOut = swapPreview.previewUnderlyingIn(MA_DAI_ADAPTER, MA_DAI_SERIES_1, PREVIEW_AMT);
+
+        emit log_named_decimal_uint("PTs out", ptsOut, 18);
+    }
 }
